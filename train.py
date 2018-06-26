a714688a8de15394988da5a9b24ebfda13180e11
MahmoudSelmy <mahmoudselmy06@gmail.com>
2018-06-26 21:54:56 +0200

    chage lr to 1e-5

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from data_preprocessing import BatchGenerator
 from DepthLoss import build_loss
 from vgg16 import Vgg16Model
-from Utills import output_predict,output_groundtruth
+from Utills import output_predict, output_groundtruth
 
 BATCH_SIZE = 12
 TRAIN_FILE = "train.csv"
@@ -23,7 +23,7 @@ MOVING_AVERAGE_DECAY = 0.999999
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 500
 NUM_EPOCHS_PER_DECAY = 30
 
-no_iterations = 21100//BATCH_SIZE + 1
+no_iterations = 21100 // BATCH_SIZE + 1
 
 Weights_DIR = 'Weights'
 SCALE2_DIR = 'Scale2'
@@ -31,8 +31,8 @@ SCALE2_DIR = 'Scale2'
 logs_path_train = './tmp/multi_scale/1/train'
 logs_path_test = './tmp/multi_scale/1/test'
 
-def train_model(continue_flag=False):
 
+def train_model(continue_flag=False):
     if not gfile.Exists(Weights_DIR):
         gfile.MakeDirs(Weights_DIR)
 
@@ -42,8 +42,9 @@ def train_model(continue_flag=False):
         with tf.device('/cpu:0'):
             batch_generator = BatchGenerator(batch_size=BATCH_SIZE)
             # train_images, train_depths, train_pixels_mask = batch_generator.csv_inputs(TRAIN_FILE)
-            train_images, train_depths, train_pixels_mask,names = batch_generator.csv_inputs(TRAIN_FILE,batch_size=BATCH_SIZE)
-            test_images, test_depths, test_pixels_mask, names = batch_generator.csv_inputs(TEST_FILE,batch_size=4)
+            train_images, train_depths, train_pixels_mask, names = batch_generator.csv_inputs(TRAIN_FILE,
+                                                                                              batch_size=BATCH_SIZE)
+            test_images, test_depths, test_pixels_mask, names = batch_generator.csv_inputs(TEST_FILE, batch_size=4)
         '''
         # placeholders
             training_images = tf.placeholder(tf.float32, shape=[None, IMAGE_HEIGHT, IMAGE_WIDTH, 3], name="training_images")
@@ -54,17 +55,17 @@ def train_model(continue_flag=False):
         # build model
         vgg = Vgg16Model()
         isTraining = tf.placeholder(tf.bool)
-        images = tf.placeholder(tf.float32, [None, 224,224,3])
-        depths = tf.placeholder(tf.float32, [None, 24,24,1])
-        pixels_masks = tf.placeholder(tf.float32, [None, 24,24,1])
+        images = tf.placeholder(tf.float32, [None, 224, 224, 3])
+        depths = tf.placeholder(tf.float32, [None, 24, 24, 1])
+        pixels_masks = tf.placeholder(tf.float32, [None, 24, 24, 1])
 
-        vgg.build(images,isTraining=isTraining)
+        vgg.build(images, isTraining=isTraining)
 
         loss = build_loss(scale2_op=vgg.outputdepth, depths=depths, pixels_mask=pixels_masks)
 
         l2_loss = 0
-        training_layers = ['conv_Pred', 'fc6', 'fc7', 'fc8','batch_normalization']
-        fine_tuing_layers = ['conv5_1','conv4_3', 'conv4_2','conv5_2','conv5_3']
+        training_layers = ['conv_Pred', 'fc6', 'fc7', 'fc8', 'batch_normalization']
+        fine_tuing_layers = ['conv5_1', 'conv4_3', 'conv4_2', 'conv5_2', 'conv5_3']
 
         trainig_params = []
         tunning_params = []
@@ -85,12 +86,11 @@ def train_model(continue_flag=False):
                     tunning_params.append(W)
                     break
 
-        loss +=  1e-4 * l2_loss
+        loss += 0.02 * l2_loss
         loss_summary = tf.summary.scalar("Loss", loss)
 
-
         tf.summary.scalar("cost", loss)
-        #learning rate
+        # learning rate
         # num_batches_per_epoch = float(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN) / BATCH_SIZE
 
 
@@ -99,23 +99,25 @@ def train_model(continue_flag=False):
 
         # decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
         lr_train = tf.train.exponential_decay(
-            1e-4,
+            1e-5,
             global_step,
-            2*no_iterations,
+            5 * no_iterations,
             0.1,
             staircase=True)
 
         lr_tune = tf.train.exponential_decay(
-            1e-4,
+            1e-5,
             global_step,
-            2*no_iterations,
+            5 * no_iterations,
             0.1,
             staircase=True)
 
-        #optimizer
-        optimizer_train = tf.train.AdamOptimizer(learning_rate=lr_train).minimize(loss, global_step=global_step,var_list=trainig_params)
-        optimizer_tune = tf.train.AdamOptimizer(learning_rate=lr_tune).minimize(loss, global_step=global_step,var_list=tunning_params)
-        optimizer = tf.group(optimizer_train,optimizer_tune)
+        # optimizer
+        optimizer_train = tf.train.AdamOptimizer(learning_rate=lr_train).minimize(loss, global_step=global_step,
+                                                                                  var_list=trainig_params)
+        optimizer_tune = tf.train.AdamOptimizer(learning_rate=lr_tune).minimize(loss, global_step=global_step,
+                                                                                var_list=tunning_params)
+        optimizer = tf.group(optimizer_train, optimizer_tune)
         # optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
         # TODO: define model saver
 
@@ -142,7 +144,9 @@ def train_model(continue_flag=False):
             # dictionary to each scale to define to seprate collections
 
             learnable_params = tf.trainable_variables()
-            not_saved_params = ['batch_normalization_3/gamma:0','batch_normalization_4/gamma:0','batch_normalization_5/gamma:0','batch_normalization_3/beta:0','batch_normalization_4/beta:0','batch_normalization_5/beta:0']
+            not_saved_params = ['batch_normalization_3/gamma:0', 'batch_normalization_4/gamma:0',
+                                'batch_normalization_5/gamma:0', 'batch_normalization_3/beta:0',
+                                'batch_normalization_4/beta:0', 'batch_normalization_5/beta:0']
 
             saved_params = []
             for v in learnable_params:
@@ -163,7 +167,6 @@ def train_model(continue_flag=False):
                 else:
                     print("No Params available")
 
-
             # initialize the queue threads to start to shovel data
             coord = tf.train.Coordinator()
             threads = tf.train.start_queue_runners(coord=coord)
@@ -173,25 +176,33 @@ def train_model(continue_flag=False):
             for epoch in range(EPOCHS):
                 for i in range(no_iterations):
 
-                    batch_images , ground_truth , batch_masks = sess.run([train_images,train_depths,train_pixels_mask])
+                    batch_images, ground_truth, batch_masks = sess.run([train_images, train_depths, train_pixels_mask])
 
-                    _, loss_value, out_depth, train_summary = sess.run([optimizer, loss, vgg.outputdepth,loss_summary]
-                                                                 ,feed_dict={images:batch_images , depths:ground_truth,pixels_masks:batch_masks,isTraining:True})
+                    _, loss_value, out_depth, train_summary = sess.run([optimizer, loss, vgg.outputdepth, loss_summary]
+                                                                       , feed_dict={images: batch_images,
+                                                                                    depths: ground_truth,
+                                                                                    pixels_masks: batch_masks,
+                                                                                    isTraining: True})
                     writer_train.add_summary(train_summary, epoch * no_iterations + i)
 
-                    if  i%10 ==0:
-                        batch_images_test, ground_truth_test, batch_masks_test = sess.run([test_images, test_depths, test_pixels_mask])
-                        validation_loss , test_summary,out_depth_test = sess.run([loss,loss_summary,vgg.outputdepth],feed_dict={images:batch_images_test , depths:ground_truth_test,pixels_masks:batch_masks_test,isTraining:False})
+                    if i % 10 == 0:
+                        batch_images_test, ground_truth_test, batch_masks_test = sess.run(
+                            [test_images, test_depths, test_pixels_mask])
+                        validation_loss, test_summary, out_depth_test = sess.run([loss, loss_summary, vgg.outputdepth],
+                                                                                 feed_dict={images: batch_images_test,
+                                                                                            depths: ground_truth_test,
+                                                                                            pixels_masks: batch_masks_test,
+                                                                                            isTraining: False})
                         writer_test.add_summary(test_summary, epoch * no_iterations + i)
-
 
                     if i % 10 == 0:
                         # log.info('step' + loss_value)
-                        print("%s: %d[epoch]: %d[iteration]: train loss %f : valid loss %f " % (datetime.now(), epoch, i, loss_value,validation_loss))
+                        print("%s: %d[epoch]: %d[iteration]: train loss %f : valid loss %f " % (
+                        datetime.now(), epoch, i, loss_value, validation_loss))
 
                     # print("%s: %d[epoch]: %d[iteration]: train loss %f" % (datetime.now(), epoch, i, loss_value))
                     if i % 500 == 0:
-                        output_groundtruth(out_depth, ground_truth,"data/predictions/train/out_%05d_%05d" % (epoch, i))
+                        output_groundtruth(out_depth, ground_truth, "data/predictions/train/out_%05d_%05d" % (epoch, i))
                         output_groundtruth(out_depth_test, ground_truth_test,
                                            "data/predictions/test/output_%05d_%05d" % (epoch, i))
                 weights_checkpoint_path = Weights_DIR + '/model'
@@ -201,8 +212,10 @@ def train_model(continue_flag=False):
             coord.join(threads)
             sess.close()
 
+
 def main(argv=None):
     train_model()
+
 
 if __name__ == '__main__':
     tf.app.run()
