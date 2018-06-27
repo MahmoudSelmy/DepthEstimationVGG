fix batch normalization updating issue

DepthLoss.py
@@ -33,7 +33,8 @@ def build_loss(scale2_op, depths, pixels_mask):
 
     # sqare_sum_d = tf.square(sum_d)
 
-    cost = tf.reduce_mean(tf.abs(d))
+    # cost = tf.reduce_mean(tf.abs(d))
+    cost = tf.reduce_mean(tf.pow(d,2))
     # cost = tf.reduce_mean( (sum_square_d / output_size ) - 0.5* (sqare_sum_d / pow(output_size,2) ))
     # cost = tf.reduce_mean(sum_square_d/output_size)
     cost = tf.sqrt(cost)

train.py
@@ -44,7 +44,7 @@ def train_model(continue_flag=False):
             # train_images, train_depths, train_pixels_mask = batch_generator.csv_inputs(TRAIN_FILE)
             train_images, train_depths, train_pixels_mask, names = batch_generator.csv_inputs(TRAIN_FILE,
                                                                                               batch_size=BATCH_SIZE)
-            test_images, test_depths, test_pixels_mask, names = batch_generator.csv_inputs(TEST_FILE, batch_size=4)
+            test_images, test_depths, test_pixels_mask, names = batch_generator.csv_inputs(TEST_FILE, batch_size=16)
         '''
         # placeholders
             training_images = tf.placeholder(tf.float32, shape=[None, IMAGE_HEIGHT, IMAGE_WIDTH, 3], name="training_images")
@@ -113,12 +113,14 @@ def train_model(continue_flag=False):
             staircase=True)
 
         # optimizer
-        optimizer_train = tf.train.AdamOptimizer(learning_rate=lr_train).minimize(loss, global_step=global_step,
-                                                                                  var_list=trainig_params)
-        optimizer_tune = tf.train.AdamOptimizer(learning_rate=lr_tune).minimize(loss, global_step=global_step,
-                                                                                var_list=tunning_params)
-        optimizer = tf.group(optimizer_train, optimizer_tune)
-        # optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            optimizer_train = tf.train.AdamOptimizer(learning_rate=lr_train).minimize(loss, global_step=global_step,
+                                                                                      var_list=trainig_params)
+            optimizer_tune = tf.train.AdamOptimizer(learning_rate=lr_tune).minimize(loss, global_step=global_step,
+                                                                                    var_list=tunning_params)
+            optimizer = tf.group(optimizer_train, optimizer_tune)
+            # optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
         # TODO: define model saver
 
         # Training session

vgg16.py
@@ -75,8 +75,8 @@ class Vgg16Model:
                                 use_bias=self.use_bias)
             if batch_norm:
                 print('norm' + name)
-                #layer = tf.layers.batch_normalization(layer,training=isTraining)
-                layer = tf.keras.layers.BatchNormalization()(layer,training= isTraining)
+                layer = tf.layers.batch_normalization(layer,training=isTraining)
+                # layer = tf.keras.layers.BatchNormalization(trainable=isTraining)(layer, isTraining=True)
             layer = self.activation_fn(layer)
 
         else :